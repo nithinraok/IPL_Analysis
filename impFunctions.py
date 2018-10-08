7d283dc8c7d9586437778c183ed56c9ee221401d
nithinraok <nithinrao.koluguri@gmail.com>
2018-10-08 15:53:54 -0700

    Added

---
+++ impFunctions.py
@@ -0,0 +1,37 @@
+def rand_by_mask(mask): return np.random.choice(np.where(mask)[0], 4, replace=False)
+def rand_by_correct(is_correct): return rand_by_mask((preds == data.val_y)==is_correct)
+
+def plot_val_with_title(idxs, title):
+    imgs = np.stack([data.val_ds[x][0] for x in idxs])
+    title_probs = [probs[x] for x in idxs]
+    print(title)
+    return plots(data.val_ds.denorm(imgs), rows=1, titles=title_probs)
+
+def plots(ims, figsize=(12,6), rows=1, titles=None):
+    f = plt.figure(figsize=figsize)
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, len(ims)//rows, i+1)
+        sp.axis('Off')
+        if titles is not None: sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i])
+
+def load_img_id(ds, idx): return np.array(PIL.Image.open(PATH+ds.fnames[idx]))
+
+def plot_val_with_title(idxs, title):
+    imgs = [load_img_id(data.val_ds,x) for x in idxs]
+    title_probs = [probs[x] for x in idxs]
+    print(title)
+    return plots(imgs, rows=1, titles=title_probs, figsize=(16,8))
+
+def most_by_mask(mask, mult):
+    idxs = np.where(mask)[0]
+    return idxs[np.argsort(mult * probs[idxs])[:4]]
+
+def most_by_correct(y, is_correct):
+    mult = -1 if (y==1)==is_correct else 1
+    return most_by_mask(((preds == data.val_y)==is_correct) & (data.val_y == y), mult)
+
+
+plot_val_with_title(most_by_correct(0, True), "Most correct cats")
+# 1. A few correct labels at random
+plot_val_with_title(rand_by_correct(True), "Correctly classified")
